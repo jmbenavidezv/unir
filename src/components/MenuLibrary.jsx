a1cd1753f7718e07f69241a1ff0135f1dd8d8e52
Johana Benavidez <Johana.benavidez@platec.co>
2023-12-09 14:17:32 -0500

    Update MenuLibrary, create function,

--- src/components/MenuLibrary.jsx
+++ src/components/MenuLibrary.jsx
@@ -1,34 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class MenuLibrary extends Component {
-    render() {
-        return (
-            <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-                <a className='navbar-brand' href='/'>
-                    Menu
-                </a>               
-                <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-                    <ul className='navbar-nav'>
-                        <li className='nav-item active'>
-                            <NavLink className='nav-link' to='/'>
-                                Biblioteca 
-                            </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' to='FormRegister'>
-                                Registrar Libro
-                            </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className= 'nav-link' to='RequestBook'>
-                                Prestamo
-                            </NavLink>
+// Componente menú de biblioteca
+const MenuLibrary = () => {
+    return (
+        // Barra de navegación hacia las diferentes opciones
+        <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+            
+            <a className='navbar-brand' href='/'>
+                Menu
+            </a>
+           
+            <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+                {/* Links de navegación */}
+                <ul className='navbar-nav'>
+                    {/* Link hacía listado de libros */}
+                    <li className='nav-item active'>
+                        <NavLink className='nav-link' to='/'>
+                            Biblioteca
+                        </NavLink>
+                    </li>
+                    {/* Link hacia registro de nuevos libros */}
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/FormRegister'>
+                            Registrar Libro
+                        </NavLink>
+                    </li>
+                    {/* Link hacia solicitudes*/}
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/RequestBook'>
+                            Prestamo
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
 
-                        </li>                      
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
-}
+export default MenuLibrary;
